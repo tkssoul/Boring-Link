ff4b1380dcb000897cdf3282bcd29b33a4f6d282
tkssoul <2495830486@qq.com>
2025-04-30 16:11:14 +0800

    中间添加工信部备案号

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,13 @@
 <template>
   <v-footer height="40" app class="footer-container">
+    <div>
+      <a
+        href="https://beian.miit.gov.cn"
+        class="text-caption text-decoration-none"
+        style="color: grey;"
+        >浙ICP备2025167963号</a
+      >
+    </div>
     <div
       class="text-caption text-disabled d-flex ga-3"
       style="position: absolute; right: 16px"
@@ -19,14 +27,13 @@
     color: rgba(25, 118, 210, 1)
 
 .footer-container
-  position: fixed
   bottom: 0
   left: 0
   width: 100%
   height: 40px
+  opacity: 0.8
   display: flex
   justify-content: center
   align-items: center
-  background-color: rgba(0, 0, 0, 0.5)
   z-index: 999
 </style>
